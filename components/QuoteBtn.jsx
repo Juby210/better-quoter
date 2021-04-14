6912d4916a1a06622876336f5d9673ad335a97a5
Juby210 <juby210@vp.pl>
2021-04-14 10:29:43 +0200

    fix icons

--- components/QuoteBtn.jsx
+++ components/QuoteBtn.jsx
@@ -1,8 +1,11 @@
-const { React, i18n: { Messages } } = require('powercord/webpack')
-const { Tooltip, Icon } = require('powercord/components')
+const { React, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack')
+const { Tooltip } = require('powercord/components')
+
+const BlockQuote = getModuleByDisplayName('BlockQuote', false)
 
 module.exports = ({ Button, onClick }) => Button && onClick ? <Tooltip className='betterQuoterBtn' text={Messages.QUOTE}>
     <Button onClick={onClick}>
-        <Icon name='BlockQuote' width='20' />
+        {/* <Icon name='BlockQuote' width='20' /> */}
+        <BlockQuote width='20' />
     </Button>
 </Tooltip> : null
